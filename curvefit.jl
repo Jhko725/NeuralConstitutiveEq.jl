d176342cbbd2a515bcfe16e7a5a93976d87a3733
Joon-Hyuk Ko <jhko725@snu.ac.kr>
2023-04-28 16:50:54 +0900

    Further curve fit experiments performed

--- curvefit.jl
+++ curvefit.jl
@@ -37,13 +37,13 @@ let
     t_data = 0.0:t_s:2*loading.t_max
     f_true = F.(t_data)
     p = plot(t_data, f_true)
-    noise = randn(Float64, size(f_true)) * 0.1
+    noise = randn(Float64, size(f_true)) * 0.04
     scatter!(t_data, f_true + noise)
 end
 ##
 let
     t_s = 2e-3
-    noise_amp = 0.1
+    noise_amp = 0.08
     t_data = 0.0:t_s:2*loading.t_max
     f_true = F.(t_data)
     noise = randn(Float64, size(f_true)) * noise_amp
@@ -88,31 +88,31 @@ function run_experiment(E₀, γ, noise_amp, t_s, app_only=true)
 end
 ##
 let
-    E₀_true, γ_true = 0.572, 0.8
+    E₀_true, γ_true = 0.572, 0.42
     f_s = 2e-3
-    noise_amps = 0:0.01:0.3
+    noise_amps = 0:0.002:0.04
     result_app = map(x -> run_experiment(E₀_true, γ_true, x, f_s, true), noise_amps) |> x -> cat(x..., dims=3)
     result_total = map(x -> run_experiment(E₀_true, γ_true, x, f_s, false), noise_amps) |> x -> cat(x..., dims=3)
-    plot_E = scatter(noise_amps, result_app[1, 1, :], yerror=result_app[1, 2, :], label="Approach only")
+    plot_E = scatter(noise_amps, result_app[1, 1, :], yerror=result_app[1, 2, :], label="Approach only", ylabel="E₀")
     scatter!(plot_E, noise_amps, result_total[1, 1, :], yerror=result_total[1, 2, :], label="Approach & Retract")
     hline!(plot_E, [E₀_true], label="Ground truth")
-    plot_γ = scatter(noise_amps, result_app[2, 1, :], yerror=result_app[2, 2, :], label="Approach only")
+    plot_γ = scatter(noise_amps, result_app[2, 1, :], yerror=result_app[2, 2, :], label="Approach only", ylabel="γ")
     scatter!(plot_γ, noise_amps, result_total[2, 1, :], yerror=result_total[2, 2, :], label="Approach & Retract")
     hline!(plot_γ, [γ_true], label="Ground truth")
-    plot(plot_E, plot_γ, layout=(1, 2))
+    plot(plot_E, plot_γ, layout=(1, 2), xlabel="Noise amplitude")
 end
 ##
 let
-    E₀_true, noise_amp = 0.572, 0.05
+    E₀_true, noise_amp = 0.572, 0.02
     f_s = 2e-3
     γ_trues = 0.0:0.05:0.995
     result_app = map(x -> run_experiment(E₀_true, x, noise_amp, f_s, true), γ_trues) |> x -> cat(x..., dims=3)
     result_total = map(x -> run_experiment(E₀_true, x, noise_amp, f_s, false), γ_trues) |> x -> cat(x..., dims=3)
-    plot_E = scatter(γ_trues, result_app[1, 1, :], yerror=result_app[1, 2, :], label="Approach only")
+    plot_E = scatter(γ_trues, result_app[1, 1, :], yerror=result_app[1, 2, :], label="Approach only", ylabel="E₀")
     scatter!(plot_E, γ_trues, result_total[1, 1, :], yerror=result_total[1, 2, :], label="Approach & Retract")
     hline!(plot_E, [E₀_true], label="Ground truth")
-    plot_γ = scatter(γ_trues, result_app[2, 1, :] - γ_trues, yerror=result_app[2, 2, :], label="Approach only")
+    plot_γ = scatter(γ_trues, result_app[2, 1, :] - γ_trues, yerror=result_app[2, 2, :], label="Approach only", ylabel="γ_fit - γ_true")
     scatter!(plot_γ, γ_trues, result_total[2, 1, :] - γ_trues, yerror=result_total[2, 2, :], label="Approach & Retract")
     hline!(plot_γ, [0.0], label="Ground truth")
-    plot(plot_E, plot_γ, layout=(1, 2))
+    plot(plot_E, plot_γ, layout=(1, 2), xlabel="γ")
 end
