21cd8f96c01ec29752bcf3ab43b3c227a95e044e
Joon-Hyuk Ko <jhko725@snu.ac.kr>
2023-07-28 20:55:45 +0900

    Refactored the ratio of variances method

--- PLR_model_fitting.py
+++ PLR_model_fitting.py
@@ -1,25 +1,23 @@
 # %%
-from configparser import ConfigParser
 from functools import partial
-import numpy as np
-from numpy import ndarray
-import xarray as xr
-from numpy.polynomial.polynomial import Polynomial
-from scipy.interpolate import interp1d
-from scipy.integrate import quad
-from scipy.optimize import root_scalar
+
 import matplotlib.pyplot as plt
+import numpy as np
 from jhelabtoolkit.io.nanosurf import nanosurf
 from jhelabtoolkit.utils.plotting import configure_matplotlib_defaults
+from scipy.integrate import quad
+from scipy.interpolate import interp1d
+from scipy.optimize import curve_fit, root_scalar
+
 from neuralconstitutive.preprocessing import (
-    process_approach_data,
+    calc_tip_distance,
     estimate_derivative,
     get_sampling_rate,
     get_z_and_defl,
-    calc_tip_distance,
+    ratio_of_variances,
+    fit_baseline_polynomial,
 )
 from neuralconstitutive.tipgeometry import Spherical
-from scipy.optimize import curve_fit
 
 configure_matplotlib_defaults()
 
@@ -28,88 +26,28 @@ filepath = (
 )
 config, data = nanosurf.read_nid(filepath)
 
-
-# %%
-def get_sampling_rate(nid_config: ConfigParser) -> float:
-    spec_config = dict(config[r"DataSet\DataSetInfos\Spec"])
-    num_points = int(spec_config["data points"])
-    # May later use the pint library to parse unitful quantites
-    modulation_time = float(spec_config["modulation time"].split(" ")[0])
-    return num_points / modulation_time
-
-
 get_sampling_rate(config)
 # %%
 forward, backward = data["spec forward"], data["spec backward"]
-
-
-# %%
-def get_z_and_defl(spectroscopy_data: xr.DataArray) -> tuple[ndarray, ndarray]:
-    piezo_z = spectroscopy_data["z-axis sensor"].to_numpy()
-    defl = spectroscopy_data["deflection"].to_numpy()
-    return piezo_z.squeeze(), defl.squeeze()
-
-
-def calc_tip_distance(piezo_z_pos: ndarray, deflection: ndarray) -> ndarray:
-    return piezo_z_pos - deflection
-
-
-def find_contact_point(deflection: ndarray, N: int) -> ndarray:
-    # Ratio of Variance
-    rov = np.array([])
-    length = np.arange(np.size(deflection))
-    rov = np.array(
-        [
-            np.append(
-                rov,
-                np.array(
-                    [
-                        np.var(deflection[i + 1 : i + N])
-                        / np.var(deflection[i - N : i - 1])
-                    ]
-                ),
-            )
-            for i in length
-        ]
-    ).flatten()
-    rov = rov[N : np.size(rov) - N]
-    idx = np.argmax(rov)
-    return rov, idx, rov[idx]
-
-
-def fit_baseline_polynomial(
-    distance: ndarray, deflection: ndarray, contact_point: float = 0.0, degree: int = 1
-) -> Polynomial:
-    pre_contact = distance <= contact_point
-    domain = (np.amin(distance), np.amax(distance))
-    return Polynomial.fit(
-        distance[pre_contact], deflection[pre_contact], deg=degree, domain=domain
-    )
-
 
 # %%
 z_fwd, defl_fwd = get_z_and_defl(forward)
 z_bwd, defl_bwd = get_z_and_defl(backward)
 dist_fwd = calc_tip_distance(z_fwd, defl_fwd)
 dist_bwd = calc_tip_distance(z_bwd, defl_bwd)
-# cp = find_contact_point(dist_fwd, defl_fwd)
 # %%
 # ROV method
 N = 10
-rov_fwd = find_contact_point(defl_fwd, N)[0]
-idx_fwd = find_contact_point(defl_fwd, N)[1]
-rov_fwd_max = find_contact_point(defl_fwd, N)[2]
-
-rov_bwd = find_contact_point(defl_bwd, N)[0]
-idx_bwd = find_contact_point(defl_bwd, N)[1]
-rov_bwd_max = find_contact_point(defl_bwd, N)[2]
+# tuple unpacking
+rov_fwd, idx_fwd = ratio_of_variances(defl_fwd, N)
+rov_bwd, idx_bwd = ratio_of_variances(defl_bwd, N)
 # %%
 fig, ax = plt.subplots(1, 1, figsize=(5, 3))
-ax.plot(dist_fwd[N : np.size(dist_fwd) - N], find_contact_point(defl_fwd, N)[0])
+ax.plot(dist_fwd, rov_fwd)
 ax.set_xlabel("Distance(forward)")
 ax.set_ylabel("ROV")
 plt.axvline(
-    dist_fwd[N + idx_fwd],
+    dist_fwd[idx_fwd],
     color="black",
     linestyle="--",
     linewidth=1.5,
@@ -125,8 +63,8 @@ ax.set_ylabel("Force(nN)")
 ax.legend()
 # %%
 # Find contact point
-cp_fwd = dist_fwd[N + idx_fwd]
-cp_bwd = dist_bwd[N + idx_bwd]
+cp_fwd = dist_fwd[idx_fwd]
+cp_bwd = dist_bwd[idx_bwd]
 print(cp_fwd, cp_bwd)
 # %%
 # Translation
