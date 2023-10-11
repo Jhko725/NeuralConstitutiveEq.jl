eae99ddc9bd19fc359b51a80f279fda098ee3d56
sungwoo306 <paul306@snu.ac.kr>
2023-10-11 15:59:49 +0900

    update

--- relaxation_spectrum.py
+++ relaxation_spectrum.py
@@ -63,21 +63,47 @@ def function_from_discrete_spectra(t: float, t_i: jax.Array, h_i: jax.Array):
     h0 = jnp.log(t_i[1]) - jnp.log(t_i[0])
     return jnp.dot(h_i * h0, jnp.exp(-t / t_i))
 # %%
-t = jnp.arange(1e-5, 10.0, 0.001)
+t = jnp.arange(1e-2, 100.0, 0.0001)
 #g = function_from_discrete_spectra(t, *spectrum())
 phi = partial(function_from_discrete_spectra, t_i=spectrum()[0],h_i=spectrum()[1])
 g = jax.vmap(phi)(t)
-plt.plot(t, g)
+fig, ax = plt.subplots(1, 1, figsize=(7,5))
+ax.plot(t, g)
+ax.set_xlabel("Time[s]")
+ax.set_ylabel("Relaxation Function[Pa]")
 # %%
 # Relaxation function curve fitting
+def PLR(t, E0,  alpha, t_prime):
+    return E0 * (1 + t / t_prime) ** (-alpha)
+
+
+def SLS(t, E0, E_inf, tau):
+    return E_inf + (E0 - E_inf) * np.exp(-(t / tau))
+
+
+def KWW(t, E0, E_inf, tau, beta):
+    return E_inf + (E0 - E_inf) * np.exp(-((t / tau) ** beta))
+
 def Fung(t, E0, C, tau1, tau2):
     return E0 * (
         (1 + C * (sc.exp1(t / tau2) - sc.exp1(t / tau1)))
         / (1 + C * np.log(tau2 / tau1))
     )
+plr = partial(PLR, t_prime=1e-5)
+popt_plr, pcov_plr = curve_fit(plr, t, g)
+popt_sls, pcov_sls = curve_fit(SLS, t, g)
+popt_kww, pcov_kww = curve_fit(KWW, t, g, maxfev=1000000000)
 popt_fung, pcov_fung = curve_fit(Fung, t, g)
-popt_fung
 #%%
+# Relaxation Function Graph(RS curve fit)
+fig, ax = plt.subplots(1, 1, figsize=(7,5))
+ax.plot(t, PLR(t, 1e-5, *popt_plr), label="PLR")
+ax.plot(t, SLS(t, *popt_sls), label="SLS")
+ax.plot(t, KWW(t, *popt_kww), label="KWW")
+ax.plot(t, Fung(t, *popt_fung), label="Fung")
+ax.plot(t, g, label="DLNM")
+# ax.set_xscale("log")
+ax.legend()
 # %%
 def DLNM_Integrand(t_, t, phi, velocity, indentation, tip):
     a = tip.alpha
@@ -133,7 +159,7 @@ def i_app(t_):
 
 
 def i_ret(t_):
-    return (v_app(t_) * 0.2) - v_ret(t_) * (t_ - 0.2)
+    return v_ret(t_-2*t_[0])
 # %%
 # F = F_app_integral(t, phi, v_app, i_app, tip)
 # %%
@@ -146,7 +172,7 @@ tip = Conical(theta)
 # Data points
 space = 201  # odd number
 idx = int((space - 1) / 2)
-t_array = np.linspace(1e-6, 10, space)
+t_array = np.linspace(1e-5, 10, space)
 t_app = t_array[: idx + 1]
 t_ret = t_array[idx:]
 t_max = t_array[idx]
@@ -215,6 +241,7 @@ F_ret = np.array(F_ret)
 F_app[np.isnan(F_app)] = 0
 F_ret[np.isnan(F_ret)] = 0
 F_total = np.append(F_app, F_ret[1:])
+F_total = F_total * 1e9
 #%%
 fig, ax = plt.subplots(1, 1, figsize=(7,5))
 ax.plot(t_array, F_total)
@@ -228,6 +255,17 @@ def PLR_constit_integand(t_, t, E0, alpha, t_prime, velocity, indentation, tip):
         * b
         * E0
         * (1 + (t - t_) / t_prime) ** (-alpha)
+        * velocity(t_)
+        * indentation(t_) ** (b - 1)
+    )
+
+def SLS_constit_integand(t_, t, E0, E_inf, tau, velocity, indentation, tip):
+    a = tip.alpha
+    b = tip.beta
+    return (
+        a
+        * b
+        * (E_inf + (E0 - E_inf) * np.exp(-(t - t_) / tau))
         * velocity(t_)
         * indentation(t_) ** (b - 1)
     )
@@ -250,6 +288,9 @@ def Fung_constit_integrand(t_, t, E0, C, tau1, tau2, velocity, indentation, tip)
 def Integrand_PLR(t_, t, E0, t_prime, alpha, velocity):
     return (E0 * (1 + (t - t_) / t_prime) ** (-alpha)) * velocity(t_)
 
+def Integrand_SLS(t_, t, E0, E_inf, tau, velocity):
+    return (E_inf + (E0 - E_inf) * np.exp(-(t - t_) / tau)) * velocity(t_)
+
 def Integrand_Fung(t_, t, E0, C, tau1, tau2, velocity):
     return (
         E0
@@ -258,13 +299,24 @@ def Integrand_Fung(t_, t, E0, C, tau1, tau2, velocity):
             / (1 + C * np.log(tau2 / tau1))
         )
     ) * velocity(t_)
-
+#%%
 def Quadrature_PLR(t1, t_, t_max_, E0_, t_prime_, alpha_, velocity_app, velocity_ret):
     integrand_app = partial(
         Integrand_PLR, E0=E0_, t=t_, t_prime=t_prime_, alpha=alpha_, velocity=velocity_app
     )
     integrand_ret = partial(
         Integrand_PLR, E0=E0_, t=t_, t_prime=t_prime_, alpha=alpha_, velocity=velocity_ret
+    )
+    quad_app = quad(integrand_app, t1, t_max_)
+    quad_ret = quad(integrand_ret, t_max_, t_)
+    return quad_app[0] + quad_ret[0]
+
+def Quadrature_SLS(t1, t_, t_max_, E0_, E_inf_, tau_, velocity_app, velocity_ret):
+    integrand_app = partial(
+        Integrand_SLS, E0=E0_, t=t_, E_inf=E_inf_, tau=tau_, velocity=velocity_app
+    )
+    integrand_ret = partial(
+        Integrand_SLS, E0=E0_, t=t_, E_inf=E_inf_, tau=tau_, velocity=velocity_ret
     )
     quad_app = quad(integrand_app, t1, t_max_)
     quad_ret = quad(integrand_ret, t_max_, t_)
@@ -315,6 +367,28 @@ def Calculation_t1_PLR(
             t1.append(t_1.root)
     return t1
 
+def Calculation_t1_SLS(
+    time_ret, t_max__, E0__, E_inf__, tau__, velocity_app__, velocity_ret__
+):
+    t1 = []
+    for i in time_ret:
+        quadrature = partial(
+            Quadrature_SLS,
+            t_=i,
+            t_max_=t_max__,
+            E0_=E0__,
+            E_inf_=E_inf__,
+            tau_=tau__,
+            velocity_app=velocity_app__,
+            velocity_ret=velocity_ret__,
+        )
+        if quadrature(t1=0) * quadrature(t1=t_max__) > 0:
+            t1.append(0)
+        else:
+            t_1 = root_scalar(quadrature, method="bisect", bracket=(0, t_max))
+            t1.append(t_1.root)
+    return t1
+
 def Calculation_t1_Fung(
     time_ret, t_max__, E0__, C__, tau1__, tau2__, velocity_app__, velocity_ret__
 ):
@@ -337,8 +411,9 @@ def Calculation_t1_Fung(
             t_1 = root_scalar(quadrature, method="bisect", bracket=(0, t_max))
             t1.append(t_1.root)
     return t1
-
-def F_total_integral(
+#%%
+# Total Force
+def F_total_integral_PLR(
     time, E0_, alpha_, max_ind, t_prime_, indentation_, tip_, velocity_app, velocity_ret
 ):
     time_app = time[: max_ind + 1]
@@ -375,7 +450,49 @@ def F_total_integral(
         F_ret.append(quad(integrand_, 0, j)[0])
 
     F = F_app + F_ret[1:]
-    return F
+    F = np.array(F) 
+    return F * 1e9
+
+def F_total_integral_SLS(
+    time, E0_, E_inf_, tau_,
+      max_ind, indentation_, tip_, velocity_app, velocity_ret
+):
+    time_app = time[: max_ind + 1]
+    time_ret = time[max_ind:]
+    t1 = Calculation_t1_SLS(
+        time_ret, time_ret[0], E0_, E_inf_, tau_, velocity_app, velocity_ret
+    )
+    F_app = []
+    for i in time_app:
+        integrand_ = partial(
+            SLS_constit_integand,
+            t=i,
+            E0=E0_,
+            E_inf=E_inf_,
+            tau=tau_,
+            velocity=velocity_app,
+            indentation=indentation_,
+            tip=tip_,
+        )
+        F_app.append(quad(integrand_, 0, i)[0])
+
+    F_ret = []
+    for i, j in zip(time_ret, t1):
+        integrand_ = partial(
+            SLS_constit_integand,
+            t=i,
+            E0=E0_,
+            E_inf=E_inf_,
+            tau=tau_,
+            velocity=velocity_app,
+            indentation=indentation_,
+            tip=tip_,
+        )
+        F_ret.append(quad(integrand_, 0, j)[0])
+
+    F = F_app + F_ret[1:]
+    F = np.array(F)
+    return F * 1e9
 
 def F_total_integral_Fung(
     time, C_, E0_, tau1_, tau2_, max_ind, indentation_, tip_, velocity_app, velocity_ret
@@ -416,10 +533,11 @@ def F_total_integral_Fung(
         F_ret.append(quad(integrand_, 0, j)[0])
 
     F = F_app + F_ret[1:]
-    return F
-
-F_total_func = partial(
-    F_total_integral,
+    F = np.array(F)
+    return F * 1e9
+#%%
+F_total_func_PLR = partial(
+    F_total_integral_PLR,
     max_ind=idx,
     t_prime_=1e-5,
     indentation_=i_app,
@@ -427,7 +545,7 @@ F_total_func = partial(
     velocity_ret=v_ret,
     tip_=tip,
 )
-#%%
+
 F_total_func_Fung = partial(
     F_total_integral_Fung,
     max_ind=idx,
@@ -436,18 +554,35 @@ F_total_func_Fung = partial(
     velocity_ret=v_ret,
     tip_=tip,
 )
+
+F_total_func_SLS = partial(
+    F_total_integral_SLS,
+    max_ind=idx,
+    indentation_=i_app,
+    velocity_app=v_app,
+    velocity_ret=v_ret,
+    tip_=tip,
+)
+
 # %%
-popt_total, pcov_total = curve_fit(F_total_func, t_array, F_total)
-#%%
+popt_total_plr, pcov_total_plr = curve_fit(F_total_func_PLR, t_array, F_total)
 popt_total_fung, pcov_total_fung = curve_fit(F_total_func_Fung, t_array, F_total)
-F_total_curvefit = np.array(F_total_func(t_array, *popt_total))
-F_total_curvefit_fung = np.array(F_total_func_Fung(t_array, *popt_total_fung))
-print(popt_total_fung, popt_total)
+popt_total_sls, pcov_total_sls = curve_fit(F_total_func_SLS, t_array, F_total)
+
+#%%
+F_total_curvefit_plr = F_total_func_PLR(t_array, *popt_total_plr)
+F_total_curvefit_fung = F_total_func_Fung(t_array, *popt_total_fung)
+F_total_curvefit_sls = F_total_func_SLS(t_array, *popt_total_sls)
+#%%
+print(popt_total_fung, popt_total_sls, popt_total_plr)
 # %%
 fig, ax = plt.subplots(1, 1, figsize=(7,5))
-ax.plot(t_array, F_total_curvefit, label = "PLR")
-ax.plot(t_array, F_total_curvefit_fung, label="Fung")
 ax.plot(t_array, F_total, label="DLNM")
+ax.plot(t_array, F_total_curvefit_plr, label = "PLR")
+ax.plot(t_array, F_total_curvefit_sls, label="SLS")
+ax.plot(t_array, F_total_curvefit_fung, label="Fung")
+ax.set_xlabel("Time[s]")
+ax.set_ylabel("Force[nN]")
 ax.legend()
 #%%
 def RS_PLR(tau, t_prime, E0, alpha):
@@ -460,14 +595,29 @@ def RS_Fung(tau, C, tau1, tau2):
     a[idx1:idx2+1] = C
     return a
 
-spectrum_PLR = RS_PLR(t_i, 1e-5, *popt_total)
-spectrum_Fung = RS_Fung(t_i, popt_fung[0], popt_fung[2], popt_fung[3])
+spectrum_PLR = RS_PLR(t_i, 1e-5, *popt_total_plr)
+spectrum_Fung = RS_Fung(t_i, popt_total_fung[0], popt_total_fung[2], popt_total_fung[3])
 #%%
+# Relaxation Function Graph
+
+#%%
+# Relaxation Time Spectrum Graph
 fig, ax = plt.subplots(1, 1, figsize=(7, 5))
 ax.plot(t_i, h_i, label="DLNM")
 ax.plot(t_i, spectrum_PLR, label="PLR")
-ax.plot(t_i, spectrum_Fung, label="Fung")
+ax.axvline(popt_total_sls[2],label="SLS")
+ax.plot(t_i, spectrum_Fung/1000, label="Fung")
 ax.set_xscale("log", base=10)
 ax.set_xlabel("Relaxation Time τ[s]")
 ax.set_ylabel("Relaxation Spectrum H(τ)[Pa]")
+ax.legend()
+# %%
+fig, ax = plt.subplots(1, 1, figsize=(7,5))
+ax.plot()
+#%%
+popt_total_plr, popt_plr
+# %%
+popt_total_sls, popt_sls
+#%%
+popt_total_fung, popt_fung
 # %%
