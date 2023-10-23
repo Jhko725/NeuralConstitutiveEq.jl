e882cce339c780e4d2c0f861157d933561fa2773
Joon-Hyuk Ko <jhko725@snu.ac.kr>
2023-10-23 23:10:31 +0900

    Initial refactoring of Ting's solution completed

--- benchmark_ting.py
+++ benchmark_ting.py
@@ -6,19 +6,13 @@ from jax import Array
 import jax.numpy as jnp
 import equinox as eqx
 import matplotlib.pyplot as plt
-import diffrax
 import jaxopt
 import optimistix as optx
 
 from neuralconstitutive.jax.integrate import integrate_to, integrate_from
 from neuralconstitutive.jax.tipgeometry import AbstractTipGeometry, Spherical
 from neuralconstitutive.constitutive import ModifiedPowerLaw
-
-
-class Indentation(eqx.Module):
-    time: Array
-    indent: Array
-    velocity: Array
+from neuralconstitutive.indentation import Indentation
 
 
 @partial(eqx.filter_vmap, in_axes=(0, None, None, None))
@@ -28,66 +22,14 @@ def force_approach(
     approach: Indentation,
     tip: AbstractTipGeometry,
 ):
-    phi_app = relaxation(t - approach.time)
-    integrand = phi_app * approach.velocity * approach.indent ** (tip.b() - 1)
-    return integrate_to(t, approach.time, integrand) * tip.a()
-
-
-# %%
-plr = ModifiedPowerLaw(572.0, 0.2, 1e-5)
-tip = Spherical(1.0)
-Dt = 1e-2
-t_app = jnp.arange(0, 101) * Dt
-t_ret = jnp.arange(100, 201) * Dt
-v_app = 10.0 * jnp.ones_like(t_app)
-v_ret = -v_app
-d_app = v_app * t_app
-d_ret = v_app * (2 * t_ret[0] - t_ret)
-app = Indentation(t_app, d_app, v_app)
-ret = Indentation(t_ret, d_ret, v_ret)
-# %%
-f_app = force_approach(app.time, plr.relaxation_function, app, tip)
-
-# %%
-plr.relaxation_spectrum()
-# %%
-fig, ax = plt.subplots(1, 1, figsize=(5, 3))
-ax.plot(app.time, f_app)
-# %%
-app2 = diffrax.LinearInterpolation(t_app, t_app * v_app)
-
-# %%
-app2.derivative(t_app)
-
-
-# %%
-@partial(eqx.filter_vmap, in_axes=(0, None, None, None))
-def force_approach2(
-    t: float,
-    relaxation: Callable[[Array], Array],
-    approach: diffrax.AbstractGlobalInterpolation,
-    tip: AbstractTipGeometry,
-):
-    ts = approach.ts
+    t_s = approach.t
     b = tip.b()
 
     @partial(eqx.filter_vmap, in_axes=(0, None))
     def force_integrand(t_, t):
-        return (
-            relaxation(t - t_)
-            * approach.derivative(t_)
-            * approach.evaluate(t_) ** (b - 1)
-        )
+        return relaxation(t - t_) * approach.v(t_) * approach.z(t_) ** (b - 1)
 
-    return integrate_to(t, ts, force_integrand(ts, t)) * tip.a()
-
-
-# %%
-f_app2 = force_approach2(app2.ts, plr.relaxation_function, app2, tip)
-
-# %%
-fig, ax = plt.subplots(1, 1, figsize=(5, 3))
-ax.plot(app.time, f_app2)
+    return integrate_to(t, t_s, force_integrand(t_s, t)) * tip.a()
 
 
 # %%
@@ -98,12 +40,17 @@ def t1_constraint(
     approach: Indentation,
     retract: Indentation,
 ) -> float:
-    t_app, t_ret = approach.time, retract.time
-    v_app, v_ret = approach.velocity, retract.velocity
-    phi_app = relaxation(t - t_app)
-    phi_ret = relaxation(t - t_ret)
-    return integrate_from(t1, t_app, phi_app * v_app) + integrate_to(
-        t, t_ret, phi_ret * v_ret
+    @partial(eqx.filter_vmap, in_axes=(0, None))
+    def app_integrand(t_, t):
+        return relaxation(t - t_) * approach.v(t_)
+
+    @partial(eqx.filter_vmap, in_axes=(0, None))
+    def ret_integrand(t_, t):
+        return relaxation(t - t_) * retract.v(t_)
+
+    t_app, t_ret = approach.t, retract.t
+    return integrate_from(t1, t_app, app_integrand(t_app, t)) + integrate_to(
+        t, t_ret, ret_integrand(t_ret, t)
     )
 
 
@@ -114,7 +61,7 @@ def find_t1(
     approach: Indentation,
     retract: Indentation,
 ) -> Array:
-    sol_exists = t1_constraint(approach.time[0], t, relaxation, approach, retract) > 0.0
+    sol_exists = t1_constraint(approach.t[0], t, relaxation, approach, retract) > 0.0
     return jnp.where(
         sol_exists, _find_t1(t, relaxation, approach, retract), jnp.asarray(0.0)
     )
@@ -128,8 +75,8 @@ def _find_t1(
 ) -> Array:
     root_finder = jaxopt.Bisection(
         optimality_fun=t1_constraint,
-        lower=approach.time[0],
-        upper=approach.time[-1],
+        lower=approach.t[0],
+        upper=approach.t[-1],
         check_bracket=False,
     )
     return root_finder.run(
@@ -140,11 +87,27 @@ def _find_t1(
     ).params
 
 
-# %%
-find_t1(ret.time, plr.relaxation_function, app, ret)
+@partial(eqx.filter_vmap, in_axes=(0, 0, None, None, None))
+def force_retract(
+    t: float,
+    t1: float,
+    relaxation: Callable[[Array], Array],
+    approach: Indentation,
+    tip: AbstractTipGeometry,
+):
+    t_s = approach.t
+    b = tip.b()
+
+    @partial(eqx.filter_vmap, in_axes=(0, None))
+    def force_integrand(t_, t):
+        return relaxation(t - t_) * approach.v(t_) * approach.z(t_) ** (b - 1)
+
+    return integrate_to(t1, t_s, force_integrand(t_s, t)) * tip.a()
 
 
 # %%
+## Attempt to use optimistix: doesn't work
+## Bisection does not properly converge at the moment...
 # @partial(eqx.filter_vmap, in_axes=(0, None, None, None))
 def find_t12(
     t: float,
@@ -177,5 +140,23 @@ def find_t12(
 
 
 # %%
-find_t12(ret.time[10], plr.relaxation_function, app, ret)
+plr = ModifiedPowerLaw(572.0, 0.2, 1e-5)
+tip = Spherical(1.0)
+Dt = 1e-2
+t_app = jnp.arange(0, 101) * Dt
+t_ret = jnp.arange(100, 201) * Dt
+d_app = 10.0 * t_app
+d_ret = 10.0 * (2 * t_ret[0] - t_ret)
+app = Indentation(t_app, d_app)
+ret = Indentation(t_ret, d_ret)
+
+# %%
+f_app = force_approach(app.t, plr.relaxation_function, app, tip)
+t1 = find_t1(ret.t, plr.relaxation_function, app, ret)
+f_ret = force_retract(ret.t, t1, plr.relaxation_function, app, tip)
+# %%
+fig, ax = plt.subplots(1, 1, figsize=(5, 3))
+ax.plot(app.t, f_app, ".")
+ax.plot(ret.t, f_ret, ".")
+
 # %%
--- neuralconstitutive/indentation.py
+++ neuralconstitutive/indentation.py
@@ -1,66 +1,22 @@
-from typing import Callable
-import abc
-from functools import cached_property
-
-from torch import nn, Tensor
-from torch.func import vmap, grad
+from jax import Array
+import equinox as eqx
+import diffrax
 
 
-class Indentation(nn.Module, abc.ABC):
-    @abc.abstractmethod
-    def indent_approach(self, t: Tensor) -> Tensor:
-        pass
+class Indentation(eqx.Module):
+    trajectory: diffrax.AbstractGlobalInterpolation
 
-    @abc.abstractmethod
-    def indent_retract(self, t: Tensor) -> Tensor:
-        pass
+    def __init__(self, time: Array, indentation: Array):
+        # TODO: add keyword argument that determines what interpolation to use
+        # TODO: check input to see if indentation is monotone increasing or not
+        self.trajectory = diffrax.LinearInterpolation(time, indentation)
 
     @property
-    @abc.abstractmethod
-    def t_max(self) -> float:
-        return None
+    def t(self) -> Array:
+        return self.trajectory.ts
 
-    @cached_property
-    def i_app(self) -> Callable[[Tensor], Tensor]:
-        def _inner(t):
-            return self.indent_approach(t)
+    def z(self, t: Array) -> Array:
+        return self.trajectory.evaluate(t)
 
-        return vmap(_inner)
-
-    @cached_property
-    def i_ret(self) -> Callable[[Tensor], Tensor]:
-        def _inner(t):
-            return self.indent_retract(t)
-
-        return vmap(_inner)
-
-    @cached_property
-    def v_app(self) -> Callable[[Tensor], Tensor]:
-        def _inner(t):
-            return self.indent_approach(t)
-
-        return vmap(grad(_inner))
-
-    @cached_property
-    def v_ret(self) -> Callable[[Tensor], Tensor]:
-        def _inner(t):
-            return self.indent_retract(t)
-
-        return vmap(grad(_inner))
-
-
-class Triangular(Indentation):
-    def __init__(self, v: float, t_max: float):
-        super().__init__()
-        self.v = v
-        self._t_max = t_max
-
-    @property
-    def t_max(self) -> float:
-        return self._t_max
-
-    def indent_approach(self, t: Tensor) -> Tensor:
-        return self.v * t
-
-    def indent_retract(self, t: Tensor) -> Tensor:
-        return self.v * (2 * self.t_max - t)
+    def v(self, t: Array) -> Array:
+        return self.trajectory.derivative(t)
