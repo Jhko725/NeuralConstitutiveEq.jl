Found and fixed nan gradient problem in integrate.py

neuralconstitutive/constitutive.py
@@ -1,12 +1,15 @@
 import abc
+from functools import partial
 
-import jax
 from jax import Array
 import jax.numpy as jnp
 from jax.scipy.special import exp1
 import equinox as eqx
 
-from .relaxation_spectrum import AbstractLogDiscreteSpectrum
+from neuralconstitutive.custom_types import FloatScalar, as_floatscalar
+from neuralconstitutive.relaxation_spectrum import AbstractLogDiscreteSpectrum
+
+floatscalar_field = partial(eqx.field, converter=as_floatscalar)
 
 
 class AbstractConstitutiveEqn(eqx.Module):
@@ -21,37 +24,37 @@ class AbstractConstitutiveEqn(eqx.Module):
 
 
 class PowerLaw(AbstractConstitutiveEqn):
-    E0: float
-    alpha: float
-    t0: float = eqx.field(static=True)
+    E0: FloatScalar = floatscalar_field()
+    alpha: FloatScalar = floatscalar_field()
+    t0: FloatScalar = floatscalar_field(static=True)
 
     def relaxation_function(self, t):
         return self.E0 * (t / self.t0) ** (-self.alpha)
 
 
 class ModifiedPowerLaw(AbstractConstitutiveEqn):
-    E0: float
-    alpha: float
-    t0: float = eqx.field(static=True)
+    E0: FloatScalar = floatscalar_field()
+    alpha: FloatScalar = floatscalar_field()
+    t0: FloatScalar = floatscalar_field(static=True)
 
     def relaxation_function(self, t):
         return self.E0 * (1 + t / self.t0) ** (-self.alpha)
 
 
 class StandardLinearSolid(AbstractConstitutiveEqn):
-    E0: float
-    E_inf: float
-    tau: float
+    E0: FloatScalar = floatscalar_field()
+    E_inf: FloatScalar = floatscalar_field()
+    tau: FloatScalar = floatscalar_field()
 
     def relaxation_function(self, t):
         return self.E_inf + (self.E0 - self.E_inf) * jnp.exp(-t / self.tau)
 
 
 class KohlrauschWilliamsWatts(AbstractConstitutiveEqn):
-    E0: float
-    E_inf: float
-    tau: float
-    beta: float
+    E0: FloatScalar = floatscalar_field()
+    E_inf: FloatScalar = floatscalar_field()
+    tau: FloatScalar = floatscalar_field()
+    beta: FloatScalar = floatscalar_field()
 
     def relaxation_function(self, t):
         exponent = (t / self.tau) ** self.beta
@@ -59,10 +62,10 @@ class KohlrauschWilliamsWatts(AbstractConstitutiveEqn):
 
 
 class Fung(AbstractConstitutiveEqn):
-    E0: float
-    tau1: float
-    tau2: float
-    C: float
+    E0: FloatScalar = floatscalar_field()
+    tau1: FloatScalar = floatscalar_field()
+    tau2: FloatScalar = floatscalar_field()
+    C: FloatScalar = floatscalar_field()
 
     def relaxation_function(self, t):
         def _exp1_diff(t_i: float) -> float:
@@ -74,7 +77,7 @@ class Fung(AbstractConstitutiveEqn):
         # Tested on an array of length 100, this is 25 times (~550ms vs 13.5s)
         # faster than the commented out vectorized version
 
-        numerator = 1 + self.C * jax.lax.map(_exp1_diff, t)
+        numerator = 1 + self.C * _exp1_diff(t)
         # numerator = 1 + self.C * (exp1(t / self.tau2) - exp1(t / self.tau1))
         denominator = 1 + self.C * jnp.log(self.tau2 / self.tau1)
         return self.E0 * numerator / denominator

neuralconstitutive/integrate.py
@@ -1,13 +1,22 @@
-from typing import Callable, Any
+from typing import Callable
 
 import jax.numpy as jnp
-from jaxtyping import PyTree
 import quadax
 
 
-def integrate(fn: Callable, lower: float, upper: float, args: PyTree[Any]):
+def integrate(fn: Callable, bounds: tuple[float, float], args: tuple):
+    """Wrap the integrator from quadax
+
+    This is to handle the case when lower == upper, in which quadax returns nan for the integral
+    Obviously, the true value is zero
+    """
+    lower, upper = bounds
+    cond = lower == upper
+    # Add some random small value to upper so that upper==lower never happens
+    # This is to avoid the nan gradient problem outlined in https://jax.readthedocs.io/en/latest/faq.html#gradients-contain-nan-where-using-where
+    upper_ = jnp.where(cond, upper + 1e-3, upper)
     return jnp.where(
-        lower == upper,
+        cond,
         0.0,
-        quadax.quadgk(fn, (lower, upper), (args,), epsabs=1e-4, epsrel=1e-4)[0],
+        quadax.quadgk(fn, (lower, upper_), args, epsabs=1e-4, epsrel=1e-4)[0],
     )

relaxation_function.py
@@ -1,13 +1,80 @@
 # %%
+# ruff: noqa: F722
 from functools import partial
+
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import quad
 from scipy.optimize import curve_fit
 from scipy.optimize import root_scalar
 import scipy.special as sc
-import matplotlib.ticker as ticker
-from neuralconstitutive.tipgeometry import Conical
+import jax
+import jax.numpy as jnp
+from jaxtyping import Array, Float
+import equinox as eqx
+import optimistix as optx
+
+from neuralconstitutive.tipgeometry import AbstractTipGeometry, Conical
+from neuralconstitutive.constitutive import (
+    AbstractConstitutiveEqn,
+    ModifiedPowerLaw,
+    StandardLinearSolid,
+    KohlrauschWilliamsWatts,
+    Fung,
+)
+from neuralconstitutive.indentation import Indentation
+from neuralconstitutive.ting import force_ting
+from neuralconstitutive.plotting import plot_relaxation_fn, plot_indentation
+
+jax.config.update("jax_enable_x64", True)
+
+
+@eqx.filter_jit
+def fit_relaxation_fn(
+    constitutive: AbstractConstitutiveEqn,
+    time: Float[Array, " len_data"],
+    relaxation: Float[Array, " len_data"],
+    solver,
+    *,
+    max_steps: int = 1024
+):
+
+    def residual(constitutive: AbstractConstitutiveEqn, args):
+        (time,) = args
+        del args
+        relaxation_pred = eqx.filter_vmap(constitutive.relaxation_function)(time)
+        return relaxation_pred - relaxation
+
+    args = (time,)
+    result = optx.least_squares(
+        residual, solver, constitutive, args, max_steps=max_steps
+    )
+    return result
+
+
+@eqx.filter_jit
+def fit_force(
+    constitutive: AbstractConstitutiveEqn,
+    tip: AbstractTipGeometry,
+    indentations: tuple[Indentation, Indentation],
+    forces: tuple[Float[Array, " len_app"], Float[Array, " len_ret"]],
+    solver,
+    *,
+    max_steps=1024
+):
+    def residual(constitutive, args):
+        tip, indentations, forces = args
+        del args
+        f_pred = jnp.concatenate(forces)
+        f_true = force_ting(constitutive, tip, *indentations)
+        f_true = jnp.concatenate(f_true)
+        return f_pred - f_true
+
+    args = (tip, indentations, forces)
+    result = optx.least_squares(
+        residual, solver, constitutive, args, max_steps=max_steps
+    )
+    return result
 
 
 # %%
@@ -23,7 +90,7 @@ def KWW(t, E0, E_inf, tau, beta):
     return E_inf + (E0 - E_inf) * np.exp(-((t / tau) ** beta))
 
 
-def Fung(t, E0, C, tau1, tau2):
+def Fung2(t, E0, C, tau1, tau2):
     return E0 * (
         (1 + C * (sc.exp1(t / tau2) - sc.exp1(t / tau1)))
         / (1 + C * np.log(tau2 / tau1))
@@ -31,22 +98,33 @@ def Fung(t, E0, C, tau1, tau2):
 
 
 # %%
-E0 = 572
-t_prime = 1e-5
-alpha = 0.2
 t = np.linspace(1e-2, 1e2, 1000)
 # %%
-plr_t = PLR(t, E0, t_prime, alpha)
-fig, ax = plt.subplots(1, 1, figsize=(7, 5))
-ax.plot(t, plr_t)
+plr = ModifiedPowerLaw(572.0, 0.2, 1e-5)
+
+fig, ax = plt.subplots(1, 1, figsize=(5, 3))
+ax = plot_relaxation_fn(ax, plr, t)
 # %%
+plr_data = eqx.filter_vmap(plr.relaxation_function)(t)
 # SLS model fitting(3 version)
-popt_sls, pcov_sls = curve_fit(SLS, t, plr_t)
-E0_sls, E_inf_sls, tau_sls = popt_sls[0], popt_sls[1], popt_sls[2]
-popt_sls
+sls = StandardLinearSolid(100.0, 1.0, 1.0)
+kww = KohlrauschWilliamsWatts(100.0, 1.0, 10.0, 1.0)
+fung = Fung(10.0, 0.01, 1.0, 1.0)
+solver = optx.LevenbergMarquardt(atol=1e-3, rtol=1e-3)
 # %%
-sls_t = SLS(t, *popt_sls)
+result = fit_relaxation_fn(sls, t, plr_data, solver)
 # %%
+result = fit_relaxation_fn(kww, t, plr_data, solver)
+# %%
+result = fit_relaxation_fn(fung, t, plr_data, solver)
+# %%
+fig, ax = plt.subplots(1, 1, figsize=(5, 3))
+ax = plot_relaxation_fn(ax, result.value, t)
+ax.plot(t, plr_data, ".", label="Ground truth")
+ax.legend()
+# %%
+## Figure out how to do curve fit with latin hypercube sampling
+
 # E0_sls1, E0_sls2, E0_sls3 = 60, 70, 80
 # E_inf_sls1, E_inf_sls2, E_inf_sls3 = 5, 25, 40
 tau_sls1, tau_sls2, tau_sls3 = 15.0, 20.0, 30.0
@@ -515,11 +593,42 @@ def Calculation_t1_Fung(
 
 
 # %%
-E_0 = E0  # Pa
-gamma = alpha
-t_prime = 1e-5  # s
-theta = (18.0 / 180.0) * np.pi
-tip = Conical(theta)
+# Make indentation (with normalized time and indentation)
+dt = 1e-2
+t_app = jnp.arange(0.0, 1.0 + dt, dt)
+t_ret = jnp.arange(1.0, 2.0 + dt, dt)
+h_app = 1.0 * t_app
+h_ret = 1.0 * (2.0 - t_ret)
+app, ret = Indentation(t_app, h_app), Indentation(t_ret, h_ret)
+fig, ax = plt.subplots(1, 1, figsize=(5, 3))
+ax = plot_indentation(ax, app, marker=".")
+ax = plot_indentation(ax, ret, marker=".")
+ax
+# %%
+tip = Conical(jnp.pi / 10)  # in radians
+plr = ModifiedPowerLaw(572.0, 0.2, 1e-5)
+
+force_plr = force_ting(plr, tip, app, ret)
+fig, ax = plt.subplots(1, 1, figsize=(5, 3))
+ax.plot(app.time, force_plr[0], ".")
+ax.plot(ret.time, force_plr[1], ".")
+# %%
+sls = StandardLinearSolid(100.0, 1.0, 1.0)
+solver = optx.LevenbergMarquardt(atol=1e-3, rtol=1e-3)
+result_sls = fit_force(sls, tip, (app, ret), force_plr, solver)
+
+
+# %%
+@eqx.filter_jit
+def force_objective(constitutive, tip, indentations, forces):
+    f_pred = jnp.concatenate(forces)
+    f_true = force_ting(constitutive, tip, *indentations)
+    f_true = jnp.concatenate(f_true)
+    return jnp.sum((f_pred - f_true) ** 2)
+
+
+val = eqx.filter_grad(force_objective)(sls, tip, (app, ret), force_plr)
+print(val)
 # %%
 E_inf, tau, beta = popt_kww
 E0_sls, E_inf_sls, tau_sls = popt_sls

test2.py
@@ -1,14 +1,13 @@
 # %%
 import jax
 import jax.numpy as jnp
+import equinox as eqx
 import matplotlib.pyplot as plt
 
-from neuralconstitutive.constitutive import (
-    PowerLaw,
-)
+from neuralconstitutive.constitutive import ModifiedPowerLaw, Fung
 from neuralconstitutive.tipgeometry import Conical
 from neuralconstitutive.indentation import Indentation
-from neuralconstitutive.ting import force_ting
+from neuralconstitutive.ting import force_ting, force_approach
 
 jax.config.update("jax_enable_x64", True)
 
@@ -20,15 +19,104 @@ app = Indentation(t, h)
 t_ret = jnp.arange(1.0, 2.0 + 1e-3, 1e-3)
 h_ret = 2.0 - t_ret
 ret = Indentation(t_ret, h_ret)
-plr = PowerLaw(1.0, 0.5, 1.0)
-# plr = ModifiedPowerLaw(1.0, -0.5, 1.0)
+# plr = PowerLaw(1.0, 0.5, 1.0)
+fung = Fung(10.0, 0.1, 10.0, 1.0)
+plr = ModifiedPowerLaw(1.0, -0.5, 1.0)
 tip = Conical(jnp.pi / 18)
 del t, h, t_ret, h_ret
 
 # %%
 f_app, f_ret = force_ting(plr, tip, app, ret)
+# f_app, f_ret = force_ting(fung, tip, app, ret)
 # %%
 fig, ax = plt.subplots(1, 1, figsize=(5, 3))
 ax.plot(app.time, f_app, ".")
 ax.plot(ret.time, f_ret, ".")
+
+
+# %%
+@eqx.filter_jit
+def test(const, tip, app, ret):
+    f_app = force_approach(const, tip, app, ret)
+    return jnp.sum(f_app)
+
+
+# %%
+value = eqx.filter_grad(test)(plr, tip, app, ret)
+print(value)
+# %%
+value.E0
+
+# %%
+import diffrax
+import quadax
+from neuralconstitutive.integrate import integrate
+
+
+# %%
+def make_force_integrand(constitutive, tip, approach_interp):
+    a, b = tip.a(), tip.b()
+
+    def dF(s, t):
+        g = constitutive.relaxation_function(t - s)
+        dh_b = (
+            b * approach_interp.derivative(s) * approach_interp.evaluate(s) ** (b - 1)
+        )
+        return a * g * dh_b
+
+    return dF
+
+
+@eqx.filter_jit
+def force_approach2(constitutive, tip, approach, retract):
+    del retract
+    app_interp = diffrax.LinearInterpolation(approach.time, approach.depth)
+
+    dF = make_force_integrand(constitutive, tip, app_interp)
+
+    def F(t):
+        return integrate(dF, (0.0, t), (t,))
+
+    return eqx.filter_vmap(F)(approach.time)
+
+
+@eqx.filter_jit
+def force_approach3(constitutive, tip, approach, retract):
+    del retract
+    app_interp = diffrax.LinearInterpolation(approach.time, approach.depth)
+
+    dF = make_force_integrand(constitutive, tip, app_interp)
+
+    def F(t):
+        return quadax.quadgk(dF, (0.0, t), (t,))[0]
+
+    return eqx.filter_vmap(F)(approach.time[1:])
+
+
+# %%
+# f1 = force_approach(plr, tip, app, ret)
+f2 = force_approach2(plr, tip, app, ret)
+
+# %%
+fig, ax = plt.subplots(1, 1, figsize=(5, 3))
+# ax.plot(app.time, f1, ".", label="Impl 1")
+ax.plot(app.time, f2, ".", label="Impl 2")
+ax
+
+
+# %%
+def test1(constitutive, tip, approach, retract):
+    return jnp.sum(force_approach(constitutive, tip, approach, retract))
+
+
+def test2(constitutive, tip, approach, retract):
+    return jnp.sum(force_approach2(constitutive, tip, approach, retract))
+
+
+# %%
+val, dval = eqx.filter_value_and_grad(test)(plr, tip, app, ret)
+print(val, dval.E0, dval.alpha, dval.t0)
+# %%
+val2, dval2 = eqx.filter_value_and_grad(test2)(plr, tip, app, ret)
+print(val2, dval2.E0, dval2.alpha, dval2.t0)
 # %%