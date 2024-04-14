0fb534f962e8b758d2e155c503119cf7ca6a5ad5
Joon-Hyuk Ko <jhko725@snu.ac.kr>
2024-04-15 01:24:42 +0900

    3 different model update

--- dataset_analysis.py
+++ dataset_analysis.py
@@ -69,7 +69,7 @@ constit_sls = StandardLinearSolid(10.0, 10.0, 10.0)
 bounds_sls = [(0.0, jnp.inf)] * 3
 
 constit_mplr = ModifiedPowerLaw(10.0, 10.0, 10.0)
-bounds_mplr = [(0.0, jnp.inf), (0.0, 1.0), (dt*1e-3, dt*1e+10)]
+bounds_mplr = [(0.0, jnp.inf), (0.0, 1.0), (0.0, jnp.inf)]
 
 constit_kww = KohlrauschWilliamsWatts(10.0, 10.0, 10.0, 10.0)
 bounds_kww = [(0.0, jnp.inf)] * 3 + [(0.0, 1.0)]
@@ -194,11 +194,6 @@ for i in tqdm(range(samples_kww.shape[0])):
     constit_fit, result = fit_approach_lmfit(constit_i, bounds_kww, tip, app, f_app)
     kww_fits.append(constit_fit)
     kww_results.append(result)
-#%%
-
-
-
-
 # %%
 ## SLS model
 fig, axes = plt.subplots(1, 2, figsize=(7, 3))
@@ -227,8 +222,7 @@ for i, (constit_fit, result) in enumerate(zip(mplr_fits, tqdm(mplr_results))):
     axes[0].plot(app.time, f_fit_app, color="gray", alpha=0.7)
     axes[0].plot(ret.time, f_fit_ret, color="gray", alpha=0.7)
 
-    axes[1] = plot_relaxation_fn(axes[1], constit_fit, app.time, color="gray")    
-
+    axes[1] = plot_relaxation_fn(axes[1], constit_fit, app.time, color="gray")
 #%%
 ## KWW model
 fig, axes = plt.subplots(1, 2, figsize=(7, 3))
@@ -242,67 +236,146 @@ for i, (constit_fit, result) in enumerate(zip(kww_fits, tqdm(kww_results))):
     axes[0].plot(app.time, f_fit_app, color="gray", alpha=0.7)
     axes[0].plot(ret.time, f_fit_ret, color="gray", alpha=0.7)
 
-    axes[1] = plot_relaxation_fn(axes[1], constit_fit, app.time, color="gray")    
-# %%
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    axes[1] = plot_relaxation_fn(axes[1], constit_fit, app.time, color="gray")
 #%%
-samples_fit = []
-for r in results:
-    samples_fit.append(list(r.params.valuesdict().values()))
-samples_fit = np.asarray(samples_fit)
+sls_samples_fit = []
+for r in sls_results:
+    sls_samples_fit.append(list(r.params.valuesdict().values()))
+sls_samples_fit = np.asarray(sls_samples_fit)
 # %%
 fig, axes = plt.subplots(1, 3, figsize=(7, 3))
 for i, ax in enumerate(axes):
     if is_logscale[i] is True:
-        s_ = samples_fit[:, i]
+        s_ = sls_samples_fit[:, i]
         s_[s_ > 0] = np.log10(s_[s_ > 0])
         ax.hist(s_)
     else:
-        ax.hist(samples_fit[:, i])
+        ax.hist(sls_samples_fit[:, i])
 # %%
-results[0].params.valuesdict()
+sls_results[0].params.valuesdict()
+#%%
+mplr_samples_fit = []
+for r in mplr_results:
+    mplr_samples_fit.append(list(r.params.valuesdict().values()))
+mplr_samples_fit = np.asarray(mplr_samples_fit)
+#%%
+fig, axes = plt.subplots(1, 3, figsize=(7, 3))
+for i, ax in enumerate(axes):
+    if is_logscale[i] is True:
+        s_ = mplr_samples_fit[:, i]
+        s_[s_ > 0] = np.log10(s_[s_ > 0])
+        ax.hist(s_)
+    else:
+        ax.hist(mplr_samples_fit[:, i])
 # %%
-constit_fit = sls_fits[8]
+mplr_results[0].params.valuesdict()
+#%%
+kww_samples_fit = []
+for r in kww_results:
+    kww_samples_fit.append(list(r.params.valuesdict().values()))
+kww_samples_fit = np.asarray(kww_samples_fit)
+#%%
+fig, axes = plt.subplots(1, 4, figsize=(7, 3))
+for i, ax in enumerate(axes):
+    if is_logscale[i] is True:
+        s_ = kww_samples_fit[:, i]
+        s_[s_ > 0] = np.log10(s_[s_ > 0])
+        ax.hist(s_)
+    else:
+        ax.hist(kww_samples_fit[:, i])
+# %%
+kww_results[0].params.valuesdict()
+#%%
+
+
+
+
+
+
+
+
+# %%
+sls_fit = sls_fits[8]
 fig, axes = plt.subplots(1, 2, figsize=(7, 3))
-f_fit_app = force_approach(constit_fit, app, tip)
-f_fit_ret = force_retract(constit_fit, (app, ret), tip)
+f_fit_app = force_approach(sls_fit, app, tip)
+f_fit_ret = force_retract(sls_fit, (app, ret), tip)
 with mpl.rc_context({"lines.markersize": 1.0, "lines.linewidth": 2.0}):
     axes[0].plot(app.time, f_app, ".", color="black", label="Data", alpha=0.5)
     axes[0].plot(ret.time, f_ret, ".", color="black", label="Data", alpha=0.5)
     axes[0].plot(app.time, f_fit_app, label="Curve fit", alpha=0.7)
     axes[0].plot(ret.time, f_fit_ret, label="Curve fit", alpha=0.7)
 
-    axes[1] = plot_relaxation_fn(axes[1], constit_fit, app.time)
+    axes[1] = plot_relaxation_fn(axes[1], sls_fit, app.time)
+
+
+mplr_fit = mplr_fits[8]
+fig, axes = plt.subplots(1, 2, figsize=(7, 3))
+f_fit_app = force_approach(mplr_fit, app, tip)
+f_fit_ret = force_retract(mplr_fit, (app, ret), tip)
+with mpl.rc_context({"lines.markersize": 1.0, "lines.linewidth": 2.0}):
+    axes[0].plot(app.time, f_app, ".", color="black", label="Data", alpha=0.5)
+    axes[0].plot(ret.time, f_ret, ".", color="black", label="Data", alpha=0.5)
+    axes[0].plot(app.time, f_fit_app, label="Curve fit", alpha=0.7)
+    axes[0].plot(ret.time, f_fit_ret, label="Curve fit", alpha=0.7)
+
+    axes[1] = plot_relaxation_fn(axes[1], mplr_fit, app.time)
+
+kww_fit = kww_fits[8]
+fig, axes = plt.subplots(1, 2, figsize=(7, 3))
+f_fit_app = force_approach(kww_fit, app, tip)
+f_fit_ret = force_retract(kww_fit, (app, ret), tip)
+with mpl.rc_context({"lines.markersize": 1.0, "lines.linewidth": 2.0}):
+    axes[0].plot(app.time, f_app, ".", color="black", label="Data", alpha=0.5)
+    axes[0].plot(ret.time, f_ret, ".", color="black", label="Data", alpha=0.5)
+    axes[0].plot(app.time, f_fit_app, label="Curve fit", alpha=0.7)
+    axes[0].plot(ret.time, f_fit_ret, label="Curve fit", alpha=0.7)
+
+    axes[1] = plot_relaxation_fn(axes[1], kww_fit, app.time)
+    
 # %%
-## Fit the entire approach-retract curve
-constit = StandardLinearSolid(10.0, 10.0, 10.0)
-bounds = [(0.0, jnp.inf)] * 3
-constit_fit, result = fit_all_lmfit(constit, bounds, tip, (app, ret), (f_app, f_ret))
+## Fit the entire approach-retract curve for SLS
+sls_fit, result = fit_all_lmfit(constit_sls, bounds_sls, tip, (app, ret), (f_app, f_ret))
 display(result)
 # %%
 fig, axes = plt.subplots(1, 2, figsize=(7, 3))
-f_fit_app = force_approach(constit_fit, app, tip)
-f_fit_ret = force_retract(constit_fit, (app, ret), tip)
+f_fit_app = force_approach(sls_fit, app, tip)
+f_fit_ret = force_retract(sls_fit, (app, ret), tip)
 with mpl.rc_context({"lines.markersize": 1.0, "lines.linewidth": 2.0}):
     axes[0].plot(app.time, f_app, ".", color="black", label="Data", alpha=0.5)
     axes[0].plot(ret.time, f_ret, ".", color="black", label="Data", alpha=0.5)
     axes[0].plot(app.time, f_fit_app, label="Curve fit", alpha=0.7)
     axes[0].plot(ret.time, f_fit_ret, label="Curve fit", alpha=0.7)
 
-    axes[1] = plot_relaxation_fn(axes[1], constit_fit, app.time)
+    axes[1] = plot_relaxation_fn(axes[1], sls_fit, app.time)
 
+# %%
+## Fit the entire approach-retract curve for SLS
+mplr_fit, result = fit_all_lmfit(constit_mplr, bounds_mplr, tip, (app, ret), (f_app, f_ret))
+display(result)
+# %%
+fig, axes = plt.subplots(1, 2, figsize=(7, 3))
+f_fit_app = force_approach(mplr_fit, app, tip)
+f_fit_ret = force_retract(mplr_fit, (app, ret), tip)
+with mpl.rc_context({"lines.markersize": 1.0, "lines.linewidth": 2.0}):
+    axes[0].plot(app.time, f_app, ".", color="black", label="Data", alpha=0.5)
+    axes[0].plot(ret.time, f_ret, ".", color="black", label="Data", alpha=0.5)
+    axes[0].plot(app.time, f_fit_app, label="Curve fit", alpha=0.7)
+    axes[0].plot(ret.time, f_fit_ret, label="Curve fit", alpha=0.7)
+
+    axes[1] = plot_relaxation_fn(axes[1], mplr_fit, app.time)
+# %%
+## Fit the entire approach-retract curve for SLS
+kww_fit, result = fit_all_lmfit(constit_kww, bounds_kww, tip, (app, ret), (f_app, f_ret))
+display(result)
+# %%
+fig, axes = plt.subplots(1, 2, figsize=(7, 3))
+f_fit_app = force_approach(kww_fit, app, tip)
+f_fit_ret = force_retract(kww_fit, (app, ret), tip)
+with mpl.rc_context({"lines.markersize": 1.0, "lines.linewidth": 2.0}):
+    axes[0].plot(app.time, f_app, ".", color="black", label="Data", alpha=0.5)
+    axes[0].plot(ret.time, f_ret, ".", color="black", label="Data", alpha=0.5)
+    axes[0].plot(app.time, f_fit_app, label="Curve fit", alpha=0.7)
+    axes[0].plot(ret.time, f_fit_ret, label="Curve fit", alpha=0.7)
+
+    axes[1] = plot_relaxation_fn(axes[1], kww_fit, app.time)
 # %%
